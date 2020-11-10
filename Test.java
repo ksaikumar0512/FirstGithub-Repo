b7a0a51ac61c905a1e0c421b752877e791dbd261
ksaikumar0512 <72634225+ksaikumar0512@users.noreply.github.com>
2020-11-09 23:05:05 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author kssai
+ *
+ */
+class Test {
+
+	/**
+	 * @param args
+	 */
+	
+	a=5;
+	b=15;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		
+		c= this.a + this.b;
+		println("test " + c);
+		
+	}
+
+}
